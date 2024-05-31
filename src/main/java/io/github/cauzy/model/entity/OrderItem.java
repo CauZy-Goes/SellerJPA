77ec3ca17c1cd39c4be78438024d91377f1b9c17
CauZy-Goes <cauafariasdev@gmail.com>
2024-05-31 09:58:05 -0300

    feat: create entities

---
+++ src/main/java/io/github/cauzy/model/entity/OrderItem.java
@@ -0,0 +1,41 @@
+package io.github.cauzy.model.entity;
+
+public class OrderItem {
+
+    private Integer id;
+    private ClientOrder order;
+    private Product product;
+    private Integer quantity;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ClientOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(ClientOrder order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
